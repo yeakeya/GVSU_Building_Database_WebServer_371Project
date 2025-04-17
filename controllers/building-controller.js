cedbd78235c78d16fc97a0d6839d3f4cb61f85d4
yeakeya <yeakeya@mail.gvsu.edu>
2025-04-17 22:10:32 +0000

    Implemented formatting for default/0 values

--- controllers/building-controller.js
+++ controllers/building-controller.js
@@ -18,7 +18,6 @@ class buildingController {
             username = "guest"
         }
         let buildings = await buildingDB.allBuildings()
-        //let buildings = [{name: "Niemeyer"},{name: "Padnos"},{name: "Kirkhof"},{name: "Kleiner"}]
         res.render("index", { buildings: buildings, username: username })
     }
 
@@ -30,7 +29,6 @@ class buildingController {
             username = "guest"
         }
         let buildings = await buildingDB.buildingFilter('campus', req.params.name)
-        //let buildings = [{name: "Niemeyer"},{name: "Padnos"},{name: "Kirkhof"},{name: "Kleiner"}]
         res.render("campus-page", { campus: req.params.name, buildings: buildings, username: username })
     }
 
@@ -42,8 +40,7 @@ class buildingController {
             username = "guest"
         }
         let building = await buildingDB.buildingFilter('name', req.params.name)
-        //let building = {name: "Niemeyer Living Center", abbreviation: "NLC", description: "The honors college. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", address: "175 Calder Dr.", yearBuilt: 1978, area: 5493, campus: "Allendale"}
-	    res.render("building-page", { building: building, username: username })
+	res.render("building-page", { building: building, username: username })
     }
 
     async renderEdit(req, res) {
@@ -70,10 +67,10 @@ class buildingController {
 
     async saveEdit(req, res) {
         let submission = new Building(req.body)
-	if (submission.yearBuilt == "") {
-	    submission.yearBuilt = 0
+	if (submission.yearbuilt == "") {
+	    submission.yearbuilt = 0
 	} else {
-            submission.yearBuilt = Number(submission.yearBuilt)
+            submission.yearbuilt = Number(submission.yearbuilt)
 	}
 	if (submission.area == "") {
 	    submission.area = 0
